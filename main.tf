7878ec0e111b80d58ffb485615c1d226ba6ae691
fkochiv <17883293+fkochiv@users.noreply.github.com>
2024-03-28 13:53:44 -0400

    Fixed backend_ prefix type in target groups.

--- main.tf
+++ main.tf
@@ -64,8 +64,8 @@ module "alb" {
   target_groups = [
     {
       name_prefix      = "blog"
-      protocol         = "HTTP"
-      port             = 80
+      backend_protocol         = "HTTP"
+      backend_port             = 80
       target_type      = "instance"
       targets = {
         target_id      = aws_instance.blog.id
